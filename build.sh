b1027804078d2ade6d50a06fda406ca9dfbd05ee
jw <jw@wstrategies.co>
2025-04-02 19:28:56 -0500

    fixed: pathing for blog posts and stylesheets

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ process_post() {
     <meta charset=\"utf-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
     <title>$title</title>
-    <link rel=\"stylesheet\" href=\"../styles.css\">
+    <link rel=\"stylesheet\" href=\"/styles.css\">
 </head>
 <body>
     <header>
